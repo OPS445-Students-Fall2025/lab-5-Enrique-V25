dee441e7d1fc1fc3d818ed27d6d147db03f88f93
Enrique Molina <emolina3@myseneca.ca>
2025-10-31 21:51:50 +0000

    I finished part b

--- lab5a.py
+++ lab5a.py
@@ -3,7 +3,7 @@
 
 def read_file_string(file_name):
     # Takes file_name as string for a file name, returns its entire contents as a string
-    f = open(str(file_name), 'r')
+    f = open('file_name', 'r')
     readFILE = f.read()
     f.close()
     return readFILE
@@ -11,13 +11,12 @@ def read_file_string(file_name):
 def read_file_list(file_name):
     # Takes a file_name as string for a file name, 
     # return its entire contents as a list of lines without new-line characters
-    f = open(str(file_name), 'r')
+    f = open('file_name', 'r')
     readlines = f.readlines()
     readlines.split('\n')
     listlines = readlines.split('\n')
     f.close()
     return listlines
-
 
 if __name__ == '__main__':
     file_name = 'data.txt'
